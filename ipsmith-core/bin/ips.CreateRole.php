daf40afe188707573a929d625be5cd0b6bb48715
Rainer Bendig <rbe@ipsmith.org>
2012-12-27 01:26:56 +0100

    Created ips.CreateRole.php script

---
+++ ipsmith-core/bin/ips.CreateRole.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Project:     IPSmith - Free ip address managing tool
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This Software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * or see http://www.ipsmith.org/docs/license
+ *
+ * For questions, help, comments, discussion, etc., please join the
+ * IPSmith mailing list. Go to http://www.ipsmith.org/lists
+ *
+ **/
+
+if(!isset($argv[1]))
+{
+	echo  "Please run \"php " .$argv[0]." rolename\"\n";
+	exit(1);
+}
+else
+{
+	include(dirname(__FILE__).'/../libs/global.php');
+
+	$checkRoleExistsQuery = "SELECT * FROM roles WHERE name=:name";
+	$checkRoleExistsStmt  = $doctrineConnection->prepare($checkRoleExistsQuery);
+
+	$checkRoleExistsStmt->bindValue('name',$argv[1]);
+
+	$checkRoleExistsStmt->execute();
+
+	if($row = $checkRoleExistsStmt->fetch())
+	{
+		echo "Role \"".$argv[1]."\" already exists.\n";
+		exit(1);
+	}
+	else
+	{
+		$createRoleQuery = "INSERT INTO roles (name, createdby) VALUES (:name, :createdby);";
+		$createRoleStmt  = $doctrineConnection->prepare($createRoleQuery);
+
+		$createRoleStmt->bindValue('name',$argv[1]);
+		$createRoleStmt->bindValue('createdby','commandline');
+
+		$createRoleStmt->execute();
+
+		echo "Role \"".$argv[1]."\" created.\n";
+		exit(0);
+	}
+}
