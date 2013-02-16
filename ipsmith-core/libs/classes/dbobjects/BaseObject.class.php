c7d82985bdb376e1e28ec9e7b04da7f940c67000
Rainer Bendig <rbe@ipsmith.org>
2013-02-16 22:34:17 +0100

    created some dbobjects

---
+++ ipsmith-core/libs/classes/dbobjects/BaseObject.class.php
@@ -0,0 +1,109 @@
+<?php
+/**
+ * Project:     IPSmith - Free ip address managing tool
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This Software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * For questions, help, comments, discussion, etc., please join the
+ * IPSmith mailing list. Go to http://www.ipsmith.org/lists
+ *
+ **/
+
+class BaseObject
+{
+	public $id = null;
+	public $guid = null;
+	public $createdat = null;
+	public $createdby = null;
+	public $modifiedat = null;
+	public $modifiedby = null;
+
+	public function IsValid()
+	{
+		return ($this->id > 0);
+	}
+
+	public function LoadMetaData($metaRow)
+	{
+		$this->id = $metaRow["id"];
+		if(isset($metaRow["guid"]) && $metaRow["guid"]!=null)
+			$this->guid = $metaRow["guid"];
+
+		if(isset($metaRow["createdat"]) && $metaRow["createdat"]!=null)
+			$this->createdat = $metaRow["createdat"];
+
+		if(isset($metaRow["createdby"]) && $metaRow["createdby"]!=null)
+			$this->createdby = $metaRow["createdby"];
+
+		if(isset($metaRow["modifiedat"]) && $metaRow["modifiedat"]!=null)
+			$this->modifiedat = $metaRow["modifiedat"];
+
+		if(isset($metaRow["modifiedby"]) && $metaRow["modifiedby"]!=null)
+			$this->modifiedby = $metaRow["modifiedby"];
+	}
+
+	public function SaveTableLog($_tablename, $_id, $_isupdate)
+	{
+		$tablelog = new TableLog();
+		$tablelog->objecttablename = $_tablename;
+		$tablelog->tableid = $_id;
+		$tablelog->username = $_SESSION["userdata"]["username"];
+
+	    if($_isupdate)
+	    {
+	    	$tablelog->action = 'ItemID #'.$_id.' updated';
+	    }
+	    else
+	    {
+	    	$tablelog->action = 'ItemID #'.$_id.' created';
+	    }
+
+	    $tablelog->Save();
+	}
+	public function SaveMetaData($_tablename, $_id, $_isupdate)
+	{
+			if($_tablename!='tablelog')
+			{
+				$this->SaveTableLog($_tablename,$_id,$_isupdate);
+			}
+
+			$metaDataQuery = "UPDATE ".$_tablename." ";
+			if($_isupdate)
+			{
+				$metaDataQuery.="SET modifiedby=:currentuser  ";
+			}
+			else
+			{
+				$metaDataQuery.= "SET createdby=:currentuser ";
+			}
+
+			if($this->guid==null)
+			{
+				$metaDataQuery.=", guid=:guid ";
+			}
+
+			$metaDataQuery.= " WHERE id=:id";
+
+			$metaDataStmt = Database::current()->prepare($metaDataQuery);
+			if($this->guid==null)
+			{
+				$metaDataStmt->bindValue('guid',GuidGenerator::Create($_tablename));
+			}
+			$metaDataStmt->bindValue('currentuser',$_SESSION["userdata"]["username"]);
+			$metaDataStmt->bindValue('id',$this->id);
+          	$metaDataStmt->execute();
+
+	}
+}
---
+++ ipsmith-core/libs/classes/dbobjects/TableLog.class.php
@@ -0,0 +1,115 @@
+<?php
+/**
+ * Project:     IPSmith - Free ip address managing tool
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This Software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * For questions, help, comments, discussion, etc., please join the
+ * IPSmith mailing list. Go to http://www.ipsmith.org/lists
+ *
+ **/
+
+class TableLog extends BaseObject
+{
+	public $tablename = "tablelog";
+
+	public $objectablename = null;
+	public $username = null;
+	public $action = null;
+	public $tableid = 0;
+
+	public function Save()
+	{
+		$isupdate = $this->IsValid();
+
+		if($this->IsValid())
+		{
+			AuditHandler::FireEvent(__METHOD__,array("id"=>$this->id));
+			$saveQuery = "UPDATE tablelog SET objecttablename=:objecttablename, tableid=:tableid, username=:username, action=:action WHERE id=:id;";
+			$saveStmt = Database::current()->prepare($saveQuery);
+			$saveStmt->bindValue('id',$this->id);
+		}
+		else
+		{
+			AuditHandler::FireEvent(__METHOD__,array("id"=>$this->id));
+			$saveQuery = "INSERT INTO tablelog (username,action,objecttablename,tableid) VALUES (:username,:action,:objecttablename,:tableid);";
+			$saveStmt = Database::current()->prepare($saveQuery);
+		}
+
+		$saveStmt->bindValue('username',$this->username);
+		$saveStmt->bindValue('action',$this->action);
+		$saveStmt->bindValue('objecttablename',$this->objecttablename);
+		$saveStmt->bindValue('tableid',$this->tableid);
+          $saveStmt->execute();
+
+		if(!$isupdate)
+		{
+			$this->id = Database::current()->lastInsertId();
+		}
+
+          $this->SaveMetaData($this->tablename, $this->id,$isupdate);
+		$this->LoadById($this->id);
+	}
+
+	public function LoadData($row)
+	{
+		//-- MetaData
+		parent::LoadMetaData($row);
+
+		//-- EntryData
+		$this->username= $row["username"];
+		$this->objecttablename= $row["objecttablename"];
+		$this->tableid = $row["tableid"];
+		$this->action= $row["action"];
+	}
+
+	public function LoadById($_id)
+	{
+		AuditHandler::FireEvent(__METHOD__,array("param-id"=>$_id));
+
+		$loadQuery = "SELECT * FROM tablelog WHERE id=:id";
+		$loadStmt = Database::current()->prepare($loadQuery);
+          $loadStmt->bindValue('id',$_id);
+          $loadStmt->execute();
+
+	    if($row = $loadStmt->fetch())
+	    {
+	    		$this->LoadData($row);
+	    }
+	}
+
+	public function LoadByTableNameAndTableId($_tablename,$_id)
+	{
+		$loadQuery = "SELECT * FROM tablelog WHERE tableid=:tableid AND objecttablename=:objecttablename";
+		$loadStmt = Database::current()->prepare($loadQuery);
+          $loadStmt->bindValue('tableid',$_id);
+          $loadStmt->bindValue('objecttablename',$_tablename);
+          $loadStmt->execute();
+          $objects = array();
+	    while($row = $loadStmt->fetch())
+	    {
+	    		$object = new TableLog();
+	    		$object->LoadById($row["id"]);
+	    		$objects[] = $object;
+	    }
+	    return $objects;
+	}
+
+	public static function GetByTableNameAndId($_tablename,$_id)
+	{
+		return self::LoadByTableNameAndTableId($_tablename, $_id);
+	}
+
+}
